de9e4dd5ee9e7c84a2e068e2ada247e12ab2e14c
mar444 <mark.zhouxili@gmail.com>
2018-03-29 00:13:08 -0700

    update

--- Chapter-4/gps.lisp
+++ Chapter-4/gps.lisp
@@ -5,10 +5,13 @@
 
 
 (defun GPS (start-state goals ops)
-  (let ((after-state-list (mappend #'(lambda (state) (apply-op state ops)) '(start-state))))
-    (if (equal-sets after-state-list '(start-state))
+  (let ((after-state-list (mappend #'(lambda (state) (apply-op state ops)) (list start-state))))
+    (if (equal-sets after-state-list (list start-state))
         (some #'(lambda (state) (every #'(lambda (goal) (member goal state)) goals)) after-state-list)
         (some #'(lambda (state) (GPS state goals ops)) after-state-list))))
+
+(defun equal-sets (x y)
+  (every #'(lambda (item) (member item y)) x))
 
 (defun apply-op (state ops)
   (cond ((null ops) (list state))
@@ -20,8 +23,5 @@
                  (apply-op state (rest ops)))))
         (t (apply-op state (rest ops)))))
 
-(print (apply-op '(a b) (list (make-op :preconds '(b) :add-list '(c) :del-list '(b)))))
-
-
-; (print (GPS '(a) '(c) (list (make-op :preconds '(b) :add-list '(c) :del-list '(b)))))
+(print (GPS '(a e) '(c e) (list (make-op :preconds '(a) :add-list '(c) :del-list '(a)))))
     
