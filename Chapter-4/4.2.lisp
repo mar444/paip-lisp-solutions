e6a649c15cb32a1be3172b2dbb743bc67fdfc6d5
mar444 <mark.zhouxili@gmail.com>
2018-04-02 00:24:23 -0700

    update

--- Chapter-4/4.2.lisp
+++ Chapter-4/4.2.lisp
@@ -1,14 +1,12 @@
 (load "auxfns.lisp")
 
 (defun permutations (lst)
-  (cond ((null lst) nil)
-        ((null (rest lst)) (list lst))
-        (t  
-    (mappend #'(lambda (item)
-        (mappend #'(lambda (permutation)
-            (list (append (list item) permutation)))
-        (permutations (remove item lst))))
-     lst))))
+  (cond ((null lst) '(()))
+        (t (mapcan #'(lambda (item)
+                      (mapcar #'(lambda (permutation)
+                                  (cons item permutation))
+                               (permutations (remove item lst))))
+                   lst))))
 
 
 (print (permutations '(1 2 3 4)))
